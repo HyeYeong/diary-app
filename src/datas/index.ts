6207490a7bdc638079b53d61da5465fa452041ae
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-02-11 20:28:36 +0900

    feat: initial datas 추가

---
+++ src/datas/index.ts
@@ -0,0 +1,18 @@
+export const initialData = [
+  {
+    id: 0,
+    title: '오늘 할 일(예시)',
+    comment: '장보기, 산책하기',
+    sort: '할일',
+    score: 4,
+    date: '2023-02-01'
+  },
+  {
+    id: 1,
+    title: '어떤 하루였는지 기록(예시)',
+    comment: '새로운 길로 산책을 다녀왔는데 좋았다.',
+    sort: '일기',
+    score: 5,
+    date: '2023-02-11'
+  },
+]
