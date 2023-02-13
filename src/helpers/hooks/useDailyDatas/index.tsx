ba8427beacdd94e41076bdd9ed73f539eedc9466
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-02-13 18:21:47 +0900

    feat: hooks추가

---
+++ src/helpers/hooks/useDailyDatas/index.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { initialData } from "@/datas/index";
+import { DailyDataItemType } from "@/helpers/common/DataTypes";
+
+export const useDailyDatas = () => {
+  const LOCAL_STORAGE_KEY = "dailyDatas";
+  const [dailyDatas, setDailyDatas] = useState<DailyDataItemType[]>(() => {
+    if (typeof window !== "undefined") {
+      const savedDailyDatas = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+      if (savedDailyDatas !== null) return JSON.parse(savedDailyDatas);
+    }
+    return initialData;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(dailyDatas));
+  }, [LOCAL_STORAGE_KEY, dailyDatas]);
+
+  return {
+    dailyDatas,
+    setDailyDatas,
+  };
+};
