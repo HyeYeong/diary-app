e4893fabe556dfbc7affeaa13cdc76e3e0645a8a
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-02-13 18:20:32 +0900

    feat: variable, default data추가

---
+++ src/helpers/common/DataTypes/index.ts
@@ -0,0 +1,7 @@
+export type DailyDataItemType = {
+  id: number;
+  title: string;
+  comment: string;
+  score: number;
+  date: string
+};
---
+++ src/styles/variables/Colors/index.ts
@@ -0,0 +1,15 @@
+export const COLORS = {
+  WHITE: '#fff',
+  BLACK: '#000',
+  BASECOLOR: '#3e3e3e',
+  GRAY: {
+    0: '#949494',
+    1: '#fbfafa',
+    2: '#eeeeee',
+  },
+  CADET_BLUE: 'cadetblue',
+  RED:'red',
+  YELLOW: '#f5bc00',
+  GREEN: '#56c165',
+  SHADOW: '#cfcfcf',
+} as const
