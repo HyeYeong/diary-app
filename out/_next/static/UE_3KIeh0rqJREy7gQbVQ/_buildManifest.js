ff4b9c6671021db17e3c23bd71923e879f3a3cef
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-05-10 18:09:25 +0900

    deploy to gh-pages

---
+++ out/_next/static/UE_3KIeh0rqJREy7gQbVQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-36f994258e23e278.js","static/chunks/804-c5b30c36af595931.js","static/chunks/pages/index-2c612693a1c1cae3.js"],"/_error":["static/chunks/pages/_error-47e526b08b70a03d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
